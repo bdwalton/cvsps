738ff49e1fa2e3f42f359b4f0856b20079f3dd86
Ben Walton <bdwalton@gmail.com>
2010-11-21 13:56:38 -0500

    reorder the gnulib handing in autoinit.sh
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

--- autoinit.sh
+++ autoinit.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+git submodule update --init $dir
+.gnulib/gnulib-tool --update
+
 aclocal -I gnulib/m4
 autoconf -I gnulib/m4
 autoheader
 automake --add-missing --foreign
-git submodule update --init $dir
-.gnulib/gnulib-tool --update
